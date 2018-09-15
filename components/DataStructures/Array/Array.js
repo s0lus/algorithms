93c6a209d42970da12592689b73f1da193c9eb5d
s0lus <mashenkoalex@gmail.com>
2018-09-15 18:35:37 +0300

    Update array data structure page

--- components/DataStructures/Array/Array.js
+++ components/DataStructures/Array/Array.js
@@ -1,6 +1,6 @@
 
 import React, { PureComponent } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import * as Data from "resources/data/ru.json";
 import array from "resources/pictures/array.png";
@@ -12,26 +12,65 @@ export default class Array extends PureComponent {
 
     render() {
         return (
-            <View>
+            <ScrollView style={styles.root} contentContainerStyle={styles.content}>
                 <Text style={styles.text}>
                     {Data.DataStructures.Array.Term}
                 </Text>
-                <Image source={array} />
-                <Text style={styles.text}>
+                <Image source={array} style={styles.image} resizeMode="contain"/>
+                <Text style={styles.typesTitle}>
                     {Data.DataStructures.Array.Types}
-                    {Data.DataStructures.Array.TypeStatic}
+                </Text>
+                <Text style={styles.typeStatic}>
+                    {Data.DataStructures.Array.TypeStatic.Title}
+                </Text>
+                <Text style={styles.termText}>
+                    {Data.DataStructures.Array.TypeStatic.Term}
+                </Text>
+                <Text style={styles.typeDynamic}>
                     {Data.DataStructures.Array.TypeDynamic.Title}
+                </Text>
+                <Text style={styles.termText}>
                     {Data.DataStructures.Array.TypeDynamic.Term}
+                </Text>
+                <Text style={styles.typeHeterogeneous}>
                     {Data.DataStructures.Array.TypeHeterogeneous.Title}
+                </Text>
+                <Text style={styles.termText}>
                     {Data.DataStructures.Array.TypeHeterogeneous.Term}
                 </Text>
-            </View>
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 14,
+    root: {
+        flex: 1,
+    },
+    content: {
+        padding: 10,
+    },
+    image: {
+        width: '100%',
+        marginVertical: 15,
+    },
+    termText: {
+        textAlign: "left",
+    },
+    typesTitle: {
+        fontSize: 18,
+        textAlign: "center",
+    },
+    typeStatic: {
+        fontSize: 16,
+        textAlign: "center",
+    },
+    typeDynamic: {
+        fontSize: 16,
+        textAlign: "center",
+    },
+    typeHeterogeneous: {
+        fontSize: 16,
+        textAlign: "center",
     }
 });
